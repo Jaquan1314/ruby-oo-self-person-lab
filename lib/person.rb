826ba81688c773f51057e4473cde1dd79e33e079
Jaquan1314 <javaughngrant@gmail.com>
2020-10-07 11:40:53 -0400

    Finished with Victor

--- lib/person.rb
+++ lib/person.rb
@@ -10,96 +10,67 @@
 # Each instance of class Person should start with eight hygiene points
 # The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and 0 respectively
 # The amount in the bank account should also be able to change, though it has no max or min.
-
+require 'pry'
 class Person
 
-    attr_accessor :bank_account
-    attr_reader :name, :happiness, :hygiene
-
+    attr_reader :name 
+    attr_accessor :happiness, :hygiene, :bank_account
+    
     def initialize(name)
-        @name = name
+        @name = name 
         @bank_account = 25
-        @happiness = 8
         @hygiene = 8
-    end
-
-
-    def happiness=(new_happiness)
-
-        if new_happiness > 10
-            @happiness = 10
-        elsif new_happiness < 0
-            @happiness = 0
-        else
-            @happiness = new_happiness
-        end
-    end
-
-    def hygiene=(new_hygiene)
-        if new_hygiene > 10
-            @hygiene = 10
-        elsif new_hygiene < 0
-            @hygiene = 0
-        else
-            @hygiene = new_hygiene
-        end
-    end
-
-    def happy?
-        if @happiness > 7
-            return true
-        else
-            return false
-        end
+        @happiness = 8
     end
 
     def clean?
-        if @hygiene > 7
-            return true
-        else
-            return false
-        end
-    end
+        hygiene > 7
+        return true 
+    end 
+
+    def happy?
+        happiness > 7 
+        return true 
+    end 
 
     def get_paid(salary)
-        @bank_account += salary
-        return "all about the benjamins"
+        bank_account += salary
+        puts "all about the benjamins"
     end
 
     def take_bath
-        hygiene_points = @hygiene += 4
-        self.hygiene=(hygiene_points)
-        return "♪ Rub-a-dub just relaxing in the tub ♫"
+        @hygiene += 4
+        puts "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.hygiene -= 3
         self.happiness += 2
-        return "♪ another one bites the dust ♫"
+        self.hygiene -= 3
+        puts "♪ another one bites the dust ♫"
     end
 
-
-    def call_friend(friend)
+    def call_friend(person)
         self.happiness += 3
-        friend.happiness += 3
-        "Hi #{friend.name}! It's #{@name}. How are you?"
+        person.happiness += 3
+        puts "Hi Felix! It's Stella. How are you?"
     end
-    
-    def start_conversation(friend,topic)
-        if topic == 'politics'
+
+    def start_conversation(person, topic)
+        if topic == "politics"
             self.happiness -= 2
-            friend.happiness -= 2
-            return 'blah blah partisan blah lobbyist'
-        elsif topic == 'weather'
-            self.happiness += 1           
-            friend.happiness += 1
-            return 'blah blah sun blah rain'
-        else topic == 'other'
-            return 'blah blah blah blah blah'
+            person.happiness -= 2 
+            return "blah blah partisan blah lobbyist"
+        elsif 
+            topic == "weather"
+            self.happiness += 100
+            person.happiness += 100   
+            return "blah blah sun blah rain"          
+        else
+            return "blah blah blah blah blah"
         end
-
     end
-
-
 end
 
+person1 = Person.new("Gon")
+person2 = Person.new("Hisoka")
+binding.pry
